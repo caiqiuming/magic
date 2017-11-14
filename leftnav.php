6534caaa75dbd3fd9e85034834313d2fb20cc141
caiqiuming <33591431+caiqiuming@users.noreply.github.com>
2017-11-14 22:10:49 +0800

    页面左侧
    页面左侧

---
+++ leftnav.php
@@ -0,0 +1,157 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>侧边导航栏</title>
+<link rel="stylesheet" type="text/css" href="css/nav.css" />
+<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>                                                                      
+<script type="text/javascript" src="js/wwwnet.js"></script>
+</head>
+<body>
+<div class="sort">
+  <div class="sort-ti">全部商品分类</div>
+  <div class="sort-list">
+    <ul>
+      <li class="">
+        <a target="view_window" class="sort-list-1">
+          <span>电视</span>
+        </a>
+        <ul>
+          <li>
+            <a href="buy_+.php?id=a1" target="view_window">创维</a>
+          </li>
+          <li>
+            <a href="buy_+.php?id=a2" target="view_window">乐视</a>
+          </li>
+          <li>
+            <a href="buy_+.php?id=a3" target="view_window">TCL</a>
+          </li>
+          <li>
+            <a href="buy_+.php?id=a4" target="view_window">康佳</a>
+          </li>
+        </ul>
+      </li>
+      <li class="">
+        <a target="view_window" class="sort-list-2">
+          <span>冰箱</span>
+        </a>
+        <ul>
+          <li>
+            <a href="buy_+.php?id=b1" target="view_window">创维</a>
+          </li>
+          <li>
+            <a href="buy_+.php?id=b2" target="view_window">海尔</a>
+          </li>
+          <li>
+            <a href="buy_+.php?id=b3" target="view_window">美菱</a>
+          </li>
+          <li>
+            <a href="buy_+.php?id=b4" target="view_window">西门子</a>
+          </li>
+        </ul>
+      </li>
+      <li class="">
+        <a  target="view_window" class="sort-list-3">
+          <span>空调</span>
+        </a>
+        <ul>
+          <li>
+            <a href="buy_+.php?id=c1" target="view_window">美的</a>
+          </li>
+          <li>
+            <a href="buy_+.php?id=c2" target="view_window">格力</a>
+          </li>
+          <li>
+            <a href="buy_+.php?id=c3" target="view_window">奥克斯</a>
+          </li>
+          <li>
+            <a href="buy_+.php?id=c4" target="view_window">TCL</a>
+          </li>
+        </ul>
+      </li>
+      <li class="">
+        <a  target="view_window" class="sort-list-4">
+          <span>洗衣机</span>
+        </a>
+        <ul>
+          <li>
+            <a href="buy_+.php?id=d1" target="view_window">小天鹅</a>
+          </li>
+          <li>
+            <a href="buy_+.php?id=d2" target="view_window">三洋</a>
+          </li>
+          <li>
+            <a href="buy_+.php?id=d3" target="view_window">美的</a>
+          </li>
+          <li>
+            <a href="buy_+.php?id=d4" target="view_window">西门子</a>
+          </li>
+
+        </ul>
+      </li>
+      <li class="">
+        <a  target="view_window" class="sort-list-5">
+          <span>热水器</span>
+        </a>
+        <ul>
+          <li>
+            <a href="buy_+.php?id=e1" target="view_window">HYUNDAI</a>
+          </li>
+          <li>
+            <a href="buy_+.php?id=e2" target="view_window">三洋</a>
+          </li>
+          <li>
+            <a href="buy_+.php?id=e3" target="view_window">夏新</a>
+          </li>
+          <li>
+            <a href="buy_+.php?id=e4" target="view_window">海尔</a>
+          </li>
+       </ul>
+      </li>
+      <li class="">
+        <a  target="view_window" class="sort-list-6">
+          <span>电饭煲</span>
+        </a>
+        <ul>
+          <li>
+            <a href="buy_+.php?id=f1" target="view_window">欧之宝</a>
+          </li>
+          <li>
+            <a href="buy_+.php?id=f2" target="view_window">半球</a>
+          </li>
+          <li>
+            <a href="buy_+.php?id=f3" target="view_window">美的</a>
+          </li>
+        </ul>
+      </li>
+      <li class="">
+        <a  target="view_window" class="sort-list-7">
+          <span>净化器</span>
+        </a>
+        <ul>
+          <li>
+            <a href="buy_+.php?id=g1" target="view_window">飞利浦</a>
+          </li>
+          <li>
+            <a href="buy_+.php?id=g2" target="view_window">三星</a>
+          </li>
+          <li>
+            <a href="buy_+.php?id=g3" target="view_window">松下</a>
+          </li>
+        </ul>
+      </li>
+      <li class="">
+        <a  target="view_window" class="sort-list-7">
+          <span>电风扇</span>
+          </a>
+        <ul>
+          <li>
+            <a href="buy_+.php?id=h1" target="view_window">荣事达</a>
+          </li>
+        </ul>        
+      </li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>
