f3eddc24d7fa1a0b38c4f93d03e893ec7c53441d
caiqiuming <33591431+caiqiuming@users.noreply.github.com>
2017-11-14 22:17:44 +0800

    页面底部
    页面底部

---
+++ footer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>家电销售平台</title>   
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/nav.css" rel="stylesheet">
+        <script type="text/javascript" src="js/wwwnet.js"></script>
+        <script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/bootstrap.js"></script> 
+    </head>
+<footer style="height:40px; width:100%; border:1px solid #0085B5; background-color:#58ACED;" >
+    <center>
+            <div style="margin-top:6px;">
+                <font style="color:#FFF;" size="4">
+                    Copyright © 2017 | CQM | All Rights Reserved   
+                </font>
+            </div>
+    </center>
+</footer>
+    </body>
+</html>
